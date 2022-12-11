5a9824a94b806556f5df29811d3a9f9bef5d632c
aknieszka <aknieszka.z@gmail.com>
2022-12-11 22:29:55 +0100

    feat: add the sytnax to check no find condition

--- element.sh
+++ element.sh
@@ -14,18 +14,28 @@ do
   if [[ $1 -eq $ATOMIC_NUMBER ]] 
   then
     echo "The element with atomic number $ATOMIC_NUMBER is "$NAME" ("$SYMBOL"). It's a "$TYPE", with a mass of $ATOMIC_MASS amu. "$NAME" has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
- break
   elif [[ $1 == "$SYMBOL" ]] 
   then 
     echo "The element with atomic number $ATOMIC_NUMBER is "$NAME" ("$SYMBOL"). It's a "$TYPE", with a mass of $ATOMIC_MASS amu. "$NAME" has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
-  break
   elif [[ $1 == "$NAME" ]] 
   then 
     echo "The element with atomic number $ATOMIC_NUMBER is "$NAME" ("$SYMBOL"). It's a "$TYPE", with a mass of $ATOMIC_MASS amu. "$NAME" has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
-  break
   fi
-done
+done 
+fi > wynik.txt
+
+PLIK=wynik.txt
+read WYNIK < $PLIK
+
+if [[ -n $WYNIK ]] 
+then
+  echo "$WYNIK"
+else 
+  echo "I could not find that element in the database."
 fi
+
+
+
 
 
 
